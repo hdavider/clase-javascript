ab5a4e87608af6e2c73fa63147a5fee723003913
hdavider <hdavider@gmail.com>
2023-08-30 20:07:54 -0600

    Proyecto de javascript

---
+++ condicionales.js
@@ -0,0 +1,9 @@
+const nombre = "Ada School";
+let edad = 123;
+let nota = 8;
+
+if (nombre === "Ada School" && nota > 7) {
+    console.log("Hola " + nombre + " su nota es = " + nota);
+} else {
+    console.log("El nombre no existe");
+}
