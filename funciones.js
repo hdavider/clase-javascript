2d97eabba2078000b144f0ab177a43488de5f6c7
hdavider <hdavider@gmail.com>
2023-08-30 21:16:56 -0600

    Con practica

---
+++ funciones.js
@@ -0,0 +1,15 @@
+const PI = 3.1415169
+function calculaAreaCircunferencia(radio){
+    let area = PI * (radio * radio);
+}
+
+
+function areaCircunferencia(radio1){
+    const pi1 = 3.1415169;
+    const resultado = pi1 * radio1 * radio1
+    //console.log(resultado)
+    return resultado
+}
+
+const result = areaCircunferencia(520);
+console.log(result);
---
+++ practica1.js
@@ -0,0 +1,19 @@
+const Positivo = "Positivo";
+const Negativo = "Negativo";
+
+
+let numero1 = 1;
+let numero2 = -20;
+
+if (numero1>=0) {
+    console.log(Positivo);
+} else {
+    console.log(Negativo);
+}
+
+if (numero2<0) {
+    console.log(Negativo);
+} else {
+    console.log(Positivo);
+}
+
